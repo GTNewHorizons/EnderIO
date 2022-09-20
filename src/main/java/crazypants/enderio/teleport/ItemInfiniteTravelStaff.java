44ab427dc8ff54b3719fde3f48ac55826e896999
boubou19 <miisterunknown@gmail.com>
2022-09-20 13:50:58 +0200

    spotless

--- src/main/java/crazypants/enderio/teleport/ItemInfiniteTravelStaff.java
+++ src/main/java/crazypants/enderio/teleport/ItemInfiniteTravelStaff.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import crazypants.enderio.EnderIO;
 import crazypants.enderio.ModObject;
 import crazypants.enderio.machine.power.PowerDisplayUtil;
+import java.util.List;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
@@ -13,85 +14,83 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-import java.util.List;
+public class ItemInfiniteTravelStaff extends ItemTravelStaff {
 
-public class ItemInfiniteTravelStaff extends ItemTravelStaff{
-
-  protected ItemInfiniteTravelStaff() {
-    super();
-    setUnlocalizedName(ModObject.itemInfiniteTravelStaff.name());
-  }
-
-  public static ItemInfiniteTravelStaff create() {
-    ItemInfiniteTravelStaff result = new ItemInfiniteTravelStaff();
-    result.init();
-    return result;
-  }
-
-  public static boolean isEquipped(EntityPlayer ep) {
-    if (ep == null || ep.getCurrentEquippedItem() == null) {
-      return false;
+    protected ItemInfiniteTravelStaff() {
+        super();
+        setUnlocalizedName(ModObject.itemInfiniteTravelStaff.name());
     }
-    return ep.getCurrentEquippedItem().getItem() == EnderIO.itemInfiniteTravelStaff;
-  }
 
-  @Override
-  protected void init() {
-    GameRegistry.registerItem(this, ModObject.itemInfiniteTravelStaff.unlocalisedName);
-  }
+    public static ItemInfiniteTravelStaff create() {
+        ItemInfiniteTravelStaff result = new ItemInfiniteTravelStaff();
+        result.init();
+        return result;
+    }
 
-  @Override
-  public void onCreated(ItemStack itemStack, World world, EntityPlayer entityPlayer) {
-    setFull(itemStack);
-  }
+    public static boolean isEquipped(EntityPlayer ep) {
+        if (ep == null || ep.getCurrentEquippedItem() == null) {
+            return false;
+        }
+        return ep.getCurrentEquippedItem().getItem() == EnderIO.itemInfiniteTravelStaff;
+    }
 
-  @Override
-  @SideOnly(Side.CLIENT)
-  public void registerIcons(IIconRegister IIconRegister) {
-    itemIcon = IIconRegister.registerIcon("enderio:itemInfiniteTravelStaff");
-  }
+    @Override
+    protected void init() {
+        GameRegistry.registerItem(this, ModObject.itemInfiniteTravelStaff.unlocalisedName);
+    }
 
-  @Override
-  public int extractEnergy(ItemStack container, int maxExtract, boolean simulate) {
-    int res = super.extractEnergy(container, maxExtract, simulate);
-    return res;
-  }
+    @Override
+    public void onCreated(ItemStack itemStack, World world, EntityPlayer entityPlayer) {
+        setFull(itemStack);
+    }
 
-  @Override
-  public void extractInternal(ItemStack item, int powerUse) {
-      return;
-  }
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister IIconRegister) {
+        itemIcon = IIconRegister.registerIcon("enderio:itemInfiniteTravelStaff");
+    }
 
-  @Override
-  @SideOnly(Side.CLIENT)
-  public void getSubItems(Item item, CreativeTabs par2CreativeTabs, List par3List) {
-    ItemStack is = new ItemStack(this);
-    setFull(is);
-    par3List.add(is);
-  }
+    @Override
+    public int extractEnergy(ItemStack container, int maxExtract, boolean simulate) {
+        int res = super.extractEnergy(container, maxExtract, simulate);
+        return res;
+    }
 
-  @Override
-  @SideOnly(Side.CLIENT)
-  public void addInformation(ItemStack itemStack, EntityPlayer par2EntityPlayer, List list, boolean par4) {
-    super.addInformation(itemStack, par2EntityPlayer, list, par4);
-    list.remove(list.size()-1); // remove the initial RF charge indicator
-    String str = "Infinite " + PowerDisplayUtil.abrevation();
-    list.add(str);
-  }
+    @Override
+    public void extractInternal(ItemStack item, int powerUse) {
+        return;
+    }
 
-  @Override
-  public String getUnlocalizedNameForTooltip(ItemStack stack) {
-    return getUnlocalizedName();
-  }
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubItems(Item item, CreativeTabs par2CreativeTabs, List par3List) {
+        ItemStack is = new ItemStack(this);
+        setFull(is);
+        par3List.add(is);
+    }
 
-  @Override
-  public boolean isActive(EntityPlayer ep, ItemStack equipped) {
-    return isEquipped(ep);
-  }
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack itemStack, EntityPlayer par2EntityPlayer, List list, boolean par4) {
+        super.addInformation(itemStack, par2EntityPlayer, list, par4);
+        list.remove(list.size() - 1); // remove the initial RF charge indicator
+        String str = "Infinite " + PowerDisplayUtil.abrevation();
+        list.add(str);
+    }
 
-  @Override
-  @SideOnly(Side.CLIENT)
-  public boolean isFull3D() {
-    return true;
-  }
+    @Override
+    public String getUnlocalizedNameForTooltip(ItemStack stack) {
+        return getUnlocalizedName();
+    }
+
+    @Override
+    public boolean isActive(EntityPlayer ep, ItemStack equipped) {
+        return isEquipped(ep);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public boolean isFull3D() {
+        return true;
+    }
 }
