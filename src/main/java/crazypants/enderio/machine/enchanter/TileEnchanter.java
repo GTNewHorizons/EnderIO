5d13495835627a565a68bbffbfac3f9b96904ce3
__felix__ <134875831+felixfour@users.noreply.github.com>
2026-07-27 11:22:41 -0400

    present null if it has null

--- src/main/java/crazypants/enderio/machine/enchanter/TileEnchanter.java
+++ src/main/java/crazypants/enderio/machine/enchanter/TileEnchanter.java
@@ -99,7 +99,7 @@ public class TileEnchanter extends TileEntityEio implements ISidedInventory {
 
     @Override
     public ItemStack getStackInSlot(int slot) {
-        if (slot == 2) return air;
+        if (slot == 2) return inv[2] != null ? air : null;
         if (slot < 0 || slot >= inv.length - 1) {
             return null;
         }
