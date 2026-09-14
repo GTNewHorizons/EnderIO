77998f1ac04037da91d15524d4ea8e0401e9c5b6
Fib4ik <channelfireblock@gmail.com>
2026-09-14 18:15:04 +0300

    added redstone signal support for item conduit.

--- src/main/java/crazypants/enderio/conduit/ConduitUtil.java
+++ src/main/java/crazypants/enderio/conduit/ConduitUtil.java
@@ -320,7 +320,7 @@ public class ConduitUtil {
         }
 
         int signalStrength = getInternalSignalForColor(bundle, col);
-        if (signalStrength < 15 && DyeColor.RED == col && bundle != null && bundle.getEntity() != null) {
+        if (signalStrength < 15 && bundle != null && bundle.getEntity() != null) {
             TileEntity te = bundle.getEntity();
             signalStrength = Math
                     .max(signalStrength, te.getWorldObj().getStrongestIndirectPower(te.xCoord, te.yCoord, te.zCoord));
