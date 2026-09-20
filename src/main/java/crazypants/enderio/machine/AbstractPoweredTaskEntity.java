a8197e0026c29bfaca35b239729b3d957acca7c0
Nikolay S. <nicksitnikov@gmail.com>
2026-09-20 06:33:30 -0500

    Remove broken NBT-matching logic (#268)

--- src/main/java/crazypants/enderio/machine/AbstractPoweredTaskEntity.java
+++ src/main/java/crazypants/enderio/machine/AbstractPoweredTaskEntity.java
@@ -52,7 +52,7 @@ public abstract class AbstractPoweredTaskEntity extends AbstractPowerConsumerEnt
 
     @Override
     public boolean isActive() {
-        return currentTask == null ? false : currentTask.getProgress() >= 0 && hasPower() && redstoneCheckPassed;
+        return currentTask != null && currentTask.getProgress() >= 0 && hasPower() && redstoneCheckPassed;
     }
 
     @Override
@@ -224,7 +224,7 @@ public abstract class AbstractPoweredTaskEntity extends AbstractPowerConsumerEnt
         ItemStack copy = item.copy();
         // try to add it to existing stacks first
         for (ItemStack outStack : outputStacks) {
-            if (outStack != null && copy != null) {
+            if (outStack != null) {
                 int num = getNumCanMerge(outStack, copy);
                 outStack.stackSize += num;
                 copy.stackSize -= num;
@@ -322,7 +322,7 @@ public abstract class AbstractPoweredTaskEntity extends AbstractPowerConsumerEnt
     }
 
     protected int getNumCanMerge(ItemStack itemStack, ItemStack result) {
-        if (!itemStack.isItemEqual(result)) {
+        if (!itemStack.isItemEqual(result) || !ItemStack.areItemStackTagsEqual(itemStack, result)) {
             return 0;
         }
         return Math.min(itemStack.getMaxStackSize() - itemStack.stackSize, result.stackSize);
